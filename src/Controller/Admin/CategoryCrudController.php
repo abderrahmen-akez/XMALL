69585a6360c27423e3ded72ef7c10e24332fc66d
akamensi <akez.muhammed@gmail.com>
2026-01-15 21:52:33 +0100

    Initial commit de XMALL - Projet e-commerce

---
+++ src/Controller/Admin/CategoryCrudController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Xmall\Controller\Admin;
+
+use Xmall\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CategoryCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Category::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Catégorie')
+            ->setEntityLabelInPlural('Catégories')
+        ;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name', 'Nom'),
+        ];
+    }
+}
---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Xmall\Controller\Admin;
+
+use Xmall\Entity\Carrier;
+use Xmall\Entity\Category;
+use Xmall\Entity\Headers;
+use Xmall\Entity\Order;
+use Xmall\Entity\Product;
+use Xmall\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /** 
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        // redirect to some CRUD controller
+        $routeBuilder = $this->get(AdminUrlGenerator::class);
+
+        return $this->redirect($routeBuilder->setController(OrderCrudController::class)->generateUrl());
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('XMALL');
+
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Produits', 'fas fa-tag', Product::class);
+        yield MenuItem::linkToCrud('Transporteurs', 'fas fa-truck', Carrier::class);
+        yield MenuItem::linkToCrud('Commandes', 'fas fa-shopping-cart', Order::class);
+        yield MenuItem::linkToCrud('Bannières', 'fas fa-desktop', Headers::class);
+    }
+}
